06b34f6ffcd6a14d6a6f0e7c632ef4f114dc5f7e
sohail-mulla <msohail.dev08@gmail.com>
2026-08-15 15:05:03 +0530

    first api program

---
+++ first-spring-api-creation/src/main/java/com/springbootproject/FirstSpringApiCreationApplication.java
@@ -0,0 +1,17 @@
+package com.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApiCreationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstSpringApiCreationApplication.class, args);
+
+		System.err.println(" Application started ......! ");
+
+	}
+
+}
